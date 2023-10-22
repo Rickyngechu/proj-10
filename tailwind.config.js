f25a9d873fc6694eb24325a1faca323148516a43
Erick ngechu <ekngechu003@gmail.com>
2023-10-22 18:06:35 +0300

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      Pink: "hsl(322, 100%, 66%)",
+
+      "Very-Pale-Cyan": "hsl(193, 100%, 96%)",
+      "Very-Dark-Cyan": "hsl(192, 100%, 9%)",
+      "Grayish-Blue": "hsl(208, 11%, 55%)",
+    },
   },
   plugins: [],
 };
