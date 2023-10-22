a6a8f5c9c5e469fc9bae9b6a872d87adcab1501e
Erick ngechu <ekngechu003@gmail.com>
2023-10-22 19:07:39 +0300

    Final updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
       "Very-Dark-Cyan": "hsl(192, 100%, 9%)",
       "Grayish-Blue": "hsl(208, 11%, 55%)",
     },
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "900px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
